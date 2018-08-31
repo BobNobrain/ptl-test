ffb7a1ed59b9ef765cc51a9716bdfafe02ffddd0
BobNobrain <nikita-ivanov.mail@yandex.ru>
2018-08-31 19:37:24 +0300

    Add client bundling

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const TestHttpServer = require('./server');
 
 const server = new TestHttpServer({
-    staticPath: path.join(__dirname, 'static'),
+    staticPath: path.join(__dirname, 'dist'),
     ptlUrl: '/ptl',
     port: process.env.PORT || 3001
 });
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log(axios);
+    alert(axios.version);
+});
